ce183f768f54d0dc85b79e104465a3a923865671
TrevPennington <43477475+TrevPennington@users.noreply.github.com>
2020-12-01 17:31:58 -0500

    fix delete link resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -11,7 +11,7 @@ const resolvers = {
         slug
       });
     },
-    async deleteLink(root, { id }, { models }) {
+    deleteLink(root, { id }, { models }) {
       return models.Link.destroy({
         where: { id }
       });
