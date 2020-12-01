dcf8471de8b93b66770227c558189e2d83fe7bda
TrevPennington <43477475+TrevPennington@users.noreply.github.com>
2020-12-01 16:32:33 -0500

    create schema / resolvers

---
+++ src/resolvers.js
@@ -0,0 +1,22 @@
+const resolvers = {
+  Query: {
+    async allLinks(root, args, { models }) {
+      return models.Link.findAll();
+    }
+  },
+  Mutation: {
+    async createLink(root, { url, slug }, { models }) {
+      return models.Link.create({
+        url,
+        slug
+      });
+    },
+    async deleteLink(root, { id }, { models }) {
+      return models.Link.destroy({
+        where: { id }
+      });
+    }
+  }
+};
+
+module.exports = resolvers;
