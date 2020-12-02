6d4bd8158c316f6c4470192f5e7dd23195065b39
TrevPennington <43477475+TrevPennington@users.noreply.github.com>
2020-12-01 19:17:31 -0500

    link creation logic

--- src/resolvers.js
+++ src/resolvers.js
@@ -6,6 +6,24 @@ const resolvers = {
   },
   Mutation: {
     async createLink(root, { url, slug }, { models }) {
+      let verifiedUrl = new URL(url);
+      let baseUrl = verifiedUrl.host;
+      s;
+      let generatedSlug = "";
+      if (slug === "") {
+        //if user did not specify a custom slug, generate one
+        var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (let i = 0; i < 4; i++)
+          generatedSlug += charset.charAt(
+            Math.floor(Math.random() * charset.length)
+          );
+        console.log(generatedSlug);
+        slug = generatedSlug;
+      }
+
+      url = `${baseUrl}/${slug}`; //new url with slug addition
+
       return models.Link.create({
         url,
         slug
